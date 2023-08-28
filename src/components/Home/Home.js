e0f9ff8f6a326482944192f9632a14bc5025b46e
ramzibouzaiene <ramzibouzaiene00@gmail.com>
2023-08-28 13:57:28 +0100

    configure the store from redux

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MovieListing from '../MovieListing/MovieListing'
+import movieApi from '../../common/apis/movieApi'
+import { APIKEY } from '../../common/apis/MovieApiKey'
 
 const Home = () => {
+  useEffect(() => {
+    const movirText = "Harry"
+    const fetchMovies = async () => {
+      const response = await movieApi.get(`?apiKey=${APIKEY}&s=${movirText}&type=movie`)
+      .catch((error) => {
+        console.error('Error :', error);
+      });
+      console.log('api response', response);
+    };
+    fetchMovies();
+  }, [])
   return (
     <div>
       <div className='banner-img'></div>
