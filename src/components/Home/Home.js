29c4637c343abb93e38df2cbb2f893ca1b411cf0
ramzibouzaiene <ramzibouzaiene00@gmail.com>
2023-08-28 17:14:39 +0100

    update the store and the style for the rest of the components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,11 @@ import React, { useEffect } from 'react'
 import MovieListing from '../MovieListing/MovieListing'
 import movieApi from '../../common/apis/movieApi'
 import { APIKEY } from '../../common/apis/MovieApiKey'
+import { useDispatch } from 'react-redux'
+import { addMovies } from '../../features/movies/movieSlice'
 
 const Home = () => {
+  const dispatch = useDispatch()
   useEffect(() => {
     const movirText = "Harry"
     const fetchMovies = async () => {
@@ -11,8 +14,8 @@ const Home = () => {
       .catch((error) => {
         console.error('Error :', error);
       });
-      console.log('api response', response);
-    };
+      dispatch(addMovies(response.data))
+     };
     fetchMovies();
   }, [])
   return (
